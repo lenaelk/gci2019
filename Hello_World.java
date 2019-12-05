11dc52fd740b0252b5808a8b8d142c99fcb646ff
lenaelk <54196042+lenaelk@users.noreply.github.com>
2019-12-04 19:27:32 -0500

    Update Hello_World.java

--- Hello_World.java
+++ Hello_World.java
@@ -1,5 +1,8 @@
 class Hello_World{
     public static void main(String [] args){
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        System.out.print("Hello World, again! How are you?");
+
+        
     }
 }
